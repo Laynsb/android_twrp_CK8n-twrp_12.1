f5949de705f91668b8073a0e59194db452c3a512
Laynsb <133554507+Laynsb@users.noreply.github.com>
2024-07-01 19:47:56 +0700

    CK8n: Allow Build TWRP Without Dependencies
    Signed-off-by: Laynsb <133554507+Laynsb@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,11 @@
 
 DEVICE_PATH := device/tecno/CK8n
 
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Inherit from mt6893-common
 include device/transsion/mt6893-common/BoardConfigCommon.mk
 
