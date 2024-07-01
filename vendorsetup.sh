38912f28de79899ea67f9d2731a27c95224f6cbd
Laynsb <133554507+Laynsb@users.noreply.github.com>
2024-07-01 14:56:45 +0700

    Fixup unable to locate repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/Laynsb/twrp-device-mt6893-common.git -b twrp-12.1 device/transsion/mt6893-common
+git clone https://github.com/Laynsb/twrp-device-mt6893-common.git -b A14 device/transsion/mt6893-common
 bash device/transsion/mt6893-common/vendorsetup.sh
